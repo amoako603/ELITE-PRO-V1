1b2fab5f7050f27f946dbd26d615c8a43d075b2f
amoako603 <amoakokelvin603@gmail.com>
2025-04-13 01:23:20 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,15 +3,15 @@ const chalk = require('chalk')
 
 
 //contact details
-global.ownernomer = "2348109263390"
-global.ownername = "ElitePro"
+global.ownernomer = "233596350252"
+global.ownername = "Amoako"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
 global.location = "Nigeria, Port Harcourt"
 
 
-global.ownernumber = '2348109263390'  //creator number
-global.ownername = 'ElitePro' //owner name
+global.ownernumber = '233596350252'  //creator number
+global.ownername = 'Amoako' //owner name
 global.botname = 'ELITE-PRO-V1' //name of the bot
 
 
